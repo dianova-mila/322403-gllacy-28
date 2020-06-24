7035fe92ba0c9feb62ad722c092f7a624b683f68
mila <dianova.mila.spb@gmail.com>
2020-06-24 13:41:17 +0300

    Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/. feedback ./
+
 var feedbackButton = document.querySelector(".map__button");
 var feedback = document.querySelector(".feedback");
 var feedbackClose = document.querySelector(".feedback__cross");
@@ -59,3 +61,91 @@ window.addEventListener("keydown", function (evt) {
       }
     }
   });
+
+/. slider ./
+
+var page = document.querySelector(".page-body");
+var slider1 = document.querySelector(".slider__item_1");
+var slider2 = document.querySelector(".slider__item_2");
+var slider3 = document.querySelector(".slider__item_3");
+var button1 = document.querySelector(".slider__button_1");
+var button2 = document.querySelector(".slider__button_2");
+var button3 = document.querySelector(".slider__button_3");
+
+button1.addEventListener("click", function () {
+    if(button2.classList.contains("slider__button_active")) {
+        button2.classList.remove("slider__button_active");
+    } else if(button3.classList.contains("slider__button_active")) {
+        button3.classList.remove("slider__button_active");
+    }
+    button1.classList.add("slider__button_active");
+    
+    if(page.classList.contains("page-body_bg_blue")) {
+        page.classList.remove("page-body_bg_blue");
+    }
+    if(page.classList.contains("page-body_bg_brown")) {
+        page.classList.remove("page-body_bg_brown");
+    }
+
+    if(slider2.classList.contains("slider__item_active")) {
+        slider2.classList.remove("slider__item_active");
+    } else if (slider3.classList.contains("slider__item_active")) {
+        slider3.classList.remove("slider__item_active");
+    } 
+    if (!slider1.classList.contains("slider__item_active")) {
+        slider1.classList.add("slider__item_active");
+    }
+});
+
+button2.addEventListener("click", function () {
+    if(button1.classList.contains("slider__button_active")) {
+        button1.classList.remove("slider__button_active");
+    } else if(button3.classList.contains("slider__button_active")) {
+        button3.classList.remove("slider__button_active");
+    }
+    button2.classList.add("slider__button_active");
+    
+    if(page.classList.contains("page-body_bg_brown")) {
+        page.classList.remove("page-body_bg_brown");
+    }
+    if(!page.classList.contains("page-body_bg_blue")) {
+        page.classList.add("page-body_bg_blue");
+    }
+
+    if(slider1.classList.contains("slider__item_active")) {
+        slider1.classList.remove("slider__item_active");
+    } else if (slider3.classList.contains("slider__item_active")) {
+        slider3.classList.remove("slider__item_active");
+    }
+
+    if(!slider2.classList.contains("slider__item_active")) {
+        slider2.classList.add("slider__item_active");
+    } 
+    
+});
+
+button3.addEventListener("click", function () {
+    if(button2.classList.contains("slider__button_active")) {
+        button2.classList.remove("slider__button_active");
+    } else if(button1.classList.contains("slider__button_active")) {
+        button1.classList.remove("slider__button_active");
+    }
+    button3.classList.add("slider__button_active");
+    
+    if(page.classList.contains("page-body_bg_blue")) {
+        page.classList.remove("page-body_bg_blue");
+    }
+    if(!page.classList.contains("page-body_bg_brown")) {
+        page.classList.add("page-body_bg_brown");
+    }
+
+    if(slider2.classList.contains("slider__item_active")) {
+        slider2.classList.remove("slider__item_active");
+    } else if (slider1.classList.contains("slider__item_active")) {
+        slider1.classList.remove("slider__item_active");
+    } 
+
+    if (!slider3.classList.contains("slider__item_active")) {
+        slider3.classList.add("slider__item_active");
+    }
+});
